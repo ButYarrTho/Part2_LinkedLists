3d94e6d629c0bebb88b3885fbb445f6de13fa1da
AlexC <alexclinton133@gmail.com>
2024-02-27 18:10:09 +0000

    Enhanced LinkedList class with additional functionality.
    Implemented a get method to retrieve the Song at a specified position in the list, handling invalid positions gracefully.
    Implemented an indexOf method to find the index of a given Song in the list.
    
    Updated the add method to throw an IllegalArgumentException when a null song is added to the list
    Updated the get method to throw an IllegalArgumentException when an invalid position is provided
    Enhanced the indexOf method to also throw an IllegalArgumentException when a null Song is provided for searching.

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -31,6 +31,10 @@ public class LinkedList {
 
     //add song to end of list
     public void add(Song song) {
+        if (song == null) {
+            throw new IllegalArgumentException("Song cannot be null.");
+        }
+
         Node newNode = new Node(song);
         if (head == null) {
             head = newNode;
@@ -42,17 +46,48 @@ public class LinkedList {
         size++;
     }
 
-    //get size of list
+    // Get the size of the list
     public int size() {
         return size;
     }
 
-    //display all songs in list
+    // Display all songs in list
     public void display() {
         Node current = head;
         while (current != null) {
             System.out.println(current.song);
             current = current.next;
         }
+    }
+
+    // Get song at a specific position
+    public Song get(int position) {
+        if (position < 0 || position >= size) {
+            throw new IllegalArgumentException("Invalid position.");
+        }
+
+        Node current = head;
+        for (int i = 0; i < position; i++) {
+            current = current.next;
+        }
+        return current.song;
+    }
+
+    // Find index of a song
+    public int indexOf(Song song) {
+        if (song == null) {
+            throw new IllegalArgumentException("Song cannot be null.");
+        }
+
+        Node current = head;
+        int index = 0;
+        while (current != null) {
+            if (current.song.equals(song)) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
     }
 }
