7599eb37d1bfd557cea2b6ccc2532972f906b0e6
AlexC <alexclinton133@gmail.com>
2024-02-27 17:47:02 +0000

    Added LinkedList class for managing Song objects.
    Implemented a Node class within LinkedList to represent each element of the list, containing a Song object and a reference to the next Node.
    Implemented a constructor to initialize the LinkedList with head, tail, and size variables.
    Implemented an add method to append a Song to the end of the list.
    Implemented a size method to retrieve the current size of the list.
    Implemented a display method to print all songs in the list."

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -8,4 +8,51 @@ import business.Song;
  */
 public class LinkedList {
 
+    private Node head;
+    private Node tail;
+    private int size;
+
+    private class Node {
+        Song song;
+        Node next;
+
+        Node(Song song) {
+            this.song = song;
+            this.next = null;
+        }
+    }
+
+    // Constructor
+    public LinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    //add song to end of list
+    public void add(Song song) {
+        Node newNode = new Node(song);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    //get size of list
+    public int size() {
+        return size;
+    }
+
+    //display all songs in list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.song);
+            current = current.next;
+        }
+    }
 }
